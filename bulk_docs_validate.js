08901d211f2315745a3661e73b77e56de5552ffa
Jason Smith (iMac) <jason.h.smith@gmail.com>
2016-05-27 15:34:44 +0700

    Refactor validation to its own file

---
+++ bulk_docs_validate.js
@@ -0,0 +1,68 @@
+module.exports = bulkDocsValidate
+
+var PouchDB = require('pouchdb')
+
+// Overwriting bulkDocs via a plugin does not work for some reason that I cannot remember.
+var bulkDocs = PouchDB.prototype.bulkDocs
+
+function bulkDocsValidate(body, options, callback) {
+  if (!callback && typeof options == 'function') {
+    callback = options
+    options = {}
+  }
+
+  var validate = this._fun.validate
+
+  if (Array.isArray(body))
+    var docs = body
+  else
+    var docs = body.docs
+
+  // Validating is easy enough. The problem is to return the result array appropriate to body.docs. In other words:
+  // bulkDocs([valid, invalid, valid], ...) should return a 3-array [ {ok:true}, {error:forbidden}, {ok:true} ], with
+  // the middle one being the validation failure. But we cannot really send that invalid doc to PouchDB.bulkDocs().
+  //
+  // So the solution is to keep an array of invalid docs, and then to rebuild the result array once bulkDocs executes.
+  var validDocs = []
+  var result = []
+  for (var i = 0; i < docs.length; i++) {
+    var doc = docs[i]
+
+    var failure = null
+    try {
+      if (validate)
+        validate(doc)
+    } catch (er) {
+      failure = er
+    }
+
+    if (failure)
+      result.push({error:true, status:403, name:'forbidden', message:failure.message, exception:failure})
+    else {
+      result.push(null)
+      validDocs.push(doc) // Send onward to bulkDocs().
+    }
+  }
+
+  // Now validDocs is a shorter list of valid documents. result is the complete list, either null or the validation error.
+  return bulkDocs.call(this, {docs:validDocs}, options, function(er, dbResult) {
+    if (er)
+      return callback(er)
+
+    // Populate the full result with the respective results for valid documents.
+    for (var i = 0; i < result.length; i++) {
+      if (!result[i]) {
+        // This document was valid. Insert its result into the result list.
+        result[i] = dbResult.shift()
+        if (!result[i])
+          return callback(new Error('Unknown error building bulkDocs result, ran out of DB results'))
+      }
+    }
+
+    if (dbResult.length != 0)
+      return callback(new Error('Unknown error building bulkDocs result, too many DB results'))
+
+    debug('bulkDocsValidate complete; %s/%s valid docs', validDocs.length, result.length)
+    callback(null, result)
+  })
+}
--- pouchdb.js
+++ pouchdb.js
@@ -7,6 +7,8 @@ var debug = require('debug')('fun-pouchdb')
 var PouchDB = require('pouchdb')
 var leveldown = require('leveldown')
 
+var bulk_docs_validate = require('./bulk_docs_validate.js')
+
 var PREFIX = __dirname + '/../../db/_pouch/'
 
 PouchDB.plugin(txn.PouchDB)
@@ -69,11 +71,11 @@ function get_db(name, options, callback) {
     if (er)
       return callback(er)
 
-    db.bulkDocs = bulkDocsValidate
-
     db._fun = {}
     db._fun.name = name
     db._fun.validate = validation_function
+
+    db.bulkDocs = bulk_docs_validate
 
     prep_ddocs(db, opts.ddocs, function(er) {
       callback(er, db)
@@ -127,67 +129,6 @@ function prep_ddocs(db, ddocs, callback) {
 }
 
 
-// Overwriting bulkDocs via a plugin does not work for some reason that I cannot remember.
-var bulkDocs = PouchDB.prototype.bulkDocs
-
-function bulkDocsValidate(body, options, callback) {
-  if (!callback && typeof options == 'function') {
-    callback = options
-    options = {}
-  }
-
-  var validate = this._fun.validate || noop
-
-  if (Array.isArray(body))
-    var docs = body
-  else
-    var docs = body.docs
-
-  // Validating is easy enough. The problem is to return the result array appropriate to body.docs. In other words:
-  // bulkDocs([valid, invalid, valid], ...) should return a 3-array [ {ok:true}, {error:forbidden}, {ok:true} ], with
-  // the middle one being the validation failure. But we cannot really send that invalid doc to PouchDB.bulkDocs().
-  //
-  // So the solution is to keep an array of invalid docs, and then to rebuild the result array once bulkDocs executes.
-  var validDocs = []
-  var result = []
-  for (var i = 0; i < docs.length; i++) {
-    var doc = docs[i]
-
-    var failure = null
-    try { validate(doc) }
-    catch (er) { failure = er }
-
-    if (failure)
-      result.push({error:true, status:403, name:'forbidden', message:failure.message, exception:failure})
-    else {
-      result.push(null)
-      validDocs.push(doc) // Send onward to bulkDocs().
-    }
-  }
-
-  // Now validDocs is a shorter list of valid documents. result is the complete list, either null or the validation error.
-  return bulkDocs.call(this, {docs:validDocs}, options, function(er, dbResult) {
-    if (er)
-      return callback(er)
-
-    // Populate the full result with the respective results for valid documents.
-    for (var i = 0; i < result.length; i++) {
-      if (!result[i]) {
-        // This document was valid. Insert its result into the result list.
-        result[i] = dbResult.shift()
-        if (!result[i])
-          return callback(new Error('Unknown error building bulkDocs result, ran out of DB results'))
-      }
-    }
-
-    if (dbResult.length != 0)
-      return callback(new Error('Unknown error building bulkDocs result, too many DB results'))
-
-    debug('bulkDocsValidate complete; %s/%s valid docs', validDocs.length, result.length)
-    callback(null, result)
-  })
-}
-
 function set_validation_function(prefix, db_name, func) {
   var key = prefix + ':' + db_name
   if (VALIDATION_CACHE[key])
@@ -202,7 +143,4 @@ function set_validation_function(prefix, db_name, func) {
 function get_validation_function(prefix, db_name) {
   var key = prefix + ':' + db_name
   return VALIDATION_CACHE[key]
-}
-
-function noop() {
 }
