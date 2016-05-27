3ce603f85b5db9d42083b306f91739081acf334a
Jason Smith (iMac) <jason.h.smith@gmail.com>
2016-05-27 15:39:41 +0700

    Move prep_ddocs to its own file

--- pouchdb.js
+++ pouchdb.js
@@ -2,15 +2,17 @@ module.exports = get_db
 module.exports.uuid = random_uuid
 
 var txn = require('txn')
-var async = require('async')
-var debug = require('debug')('fun-pouchdb')
+var debug = require('debug')('fun-pouchdb:api')
 var PouchDB = require('pouchdb')
 var leveldown = require('leveldown')
 
+var prep_ddocs = require('./prep_ddocs.js')
 var bulk_docs_validate = require('./bulk_docs_validate.js')
 
 var PREFIX = __dirname + '/../../db/_pouch/'
 
+
+// Fun PouchDB includes Txn, because Txn is nice.
 PouchDB.plugin(txn.PouchDB)
 
 
@@ -82,52 +84,6 @@ function get_db(name, options, callback) {
     })
   })
 }
-
-function prep_ddocs(db, ddocs, callback) {
-  var db_name = this._fun.name
-  debug('Prepare %s design documents for DB: %s', ddocs.length, db_name)
-  async.forEach(ddocs, prep_ddoc, ddocs_prepared)
-
-  function prep_ddoc(ddoc, to_async) {
-    debug('Prepare ddoc: %s/%s', db_name, ddoc._id)
-    db.txn({id:ddoc._id, create:true}, populate_ddoc, ddoc_stored)
-
-    function populate_ddoc(doc, to_txn) {
-      for (var key in ddoc)
-        doc[key] = ddoc[key]
-
-      var views = doc.views
-      for (var view_name in views) {
-        var view = views[vew_name]
-
-        if (typeof view.map == 'function')
-          view.map = view.map.toString()
-        if (typeof view.reduce == 'function')
-          view.reduce = view.reduce.toString()
-      }
-
-      return to_txn()
-    }
-
-    function ddoc_stored(er, new_ddoc, txr) {
-      if (er)
-        return to_async(er)
-
-      var result = (txr.stores == 0) ? 'No change' : 'Updated'
-      debug('Prepared ddoc %s/%s: %s', db_name, doc._id, result)
-      return to_async(null)
-    }
-  }
-
-  function ddocs_prepared(er) {
-    if (er)
-      return callback(er)
-
-    debug('Completed preparing %s design documents in DB: %s', this._fun.name)
-    callback()
-  }
-}
-
 
 function set_validation_function(prefix, db_name, func) {
   var key = prefix + ':' + db_name
